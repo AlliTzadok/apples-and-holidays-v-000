5669059a367ac48e4d36151613fe2568e49d87e6
Alli Tzadok <allisontzadok@gmail.com>
2018-01-16 18:00:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |seasons, holidays|
     holidays.collect do |holiday, supplies|
-      if supplies.include?("BBQ") && supplies != NIL
+      if supplies.include?("BBQ") && supplies != nil
         holiday
       end
     end.flatten
